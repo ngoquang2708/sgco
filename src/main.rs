2fc233c8bf76b5ab3b2c20dde238d24571123c80
Quang Ngô <ngoquang2708@gmail.com>
2026-05-20 01:33:36 +0700

    Handle the initial state when no configuration files have been created

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn run() -> anyhow::Result<()> {
         None => DEFAULT_OVERLAYS
             .get(&envars.app_id)
             .cloned()
-            .context(format!("No overlay config found for SteamAppId={}!", envars.app_id))?,
+            .with_context(|| format!("No config found for SteamAppId={}!", envars.app_id))?,
     };
     let app_cfg_dir = replace_placeholders(&envars, &steam_id, &overlay);
     let overlay_cfg_dir = BaseDirs::new()
@@ -42,18 +42,41 @@ fn run() -> anyhow::Result<()> {
         .join(format!("{} - {}", envars.app_id, overlay.app_name));
     let mut bwrap = Command::new("bwrap");
     bwrap.arg("--dev-bind").arg("/").arg("/");
+    let mut new_cfgs = Vec::new();
     for file in &overlay.cfg_files {
         let app_cfg = app_cfg_dir.join(file);
         let overlay_cfg = overlay_cfg_dir.join(file);
         if let Some(parent) = overlay_cfg.parent() {
-            fs::create_dir_all(parent)?;
+            fs::create_dir_all(parent).context("create overlay dirs")?;
         }
-        if !overlay_cfg.exists() {
-            fs::copy(&app_cfg, &overlay_cfg)?;
+        match (app_cfg.exists(), overlay_cfg.exists()) {
+            (_, true) => {
+                // We have the overlay file
+            }
+            (true, false) => {
+                // There is existing config file and we don't have its overlay
+                fs::copy(&app_cfg, &overlay_cfg).context("copy initial overlay file")?;
+            }
+            (false, false) => {
+                // No config file or its overlay file, skip binding and copy to new overlay later
+                new_cfgs.push(file.as_str());
+                continue;
+            }
         }
         bwrap.arg("--bind").arg(overlay_cfg).arg(app_cfg);
     }
     bwrap.arg("--").args(cli_args.cmd);
-    let _ = bwrap.spawn()?.wait()?;
+    let _ = bwrap
+        .spawn()
+        .context("spawn child process")?
+        .wait()
+        .context("wait for child process to exit")?;
+    for file in new_cfgs {
+        let app_cfg = app_cfg_dir.join(file);
+        if app_cfg.exists() {
+            fs::copy(&app_cfg, overlay_cfg_dir.join(file))
+                .context("copy newly created config file")?;
+        }
+    }
     Ok(())
 }
